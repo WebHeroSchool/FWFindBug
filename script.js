ab464a43a345966bc0f51b320d3a1a283ebc3ea1
Анна Кучерявенкова <anya.shurshalova@mail.ru>
2020-10-25 17:34:03 +0400

    изменения 1

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-function cursorPlaceOnLevel() {
-	let rhombes = document.querySelectorAll('.container__rhomb');
-	let texts = document.querySelectorAll('.container__text');
- 	rhombes.forEach(rhomb => rhomb.classList.add('container__rhomb-change'));
- 	texts.forEach(text => text.classList.add('container__text-change'));
- };
-function cursorRemoveOnLevel() {
-	let rhombes = document.querySelectorAll('.container__rhomb');
-	let texts = document.querySelectorAll('.container__text');
- 	rhombes.forEach(rhomb => rhomb.classList.remove('container__rhomb-change'));
- 	texts.forEach(text => text.classList.remove('container__text-change'));
-};
+// function cursorPlaceOnLevel() {
+// 	let rhombes = document.querySelectorAll('.container__rhomb');
+// 	let texts = document.querySelectorAll('.container__text');
+//  	rhombes.forEach(rhomb => rhomb.classList.add('container__rhomb-change'));
+//  	texts.forEach(text => text.classList.add('container__text-change'));
+//  };
+// function cursorRemoveOnLevel() {
+// 	let rhombes = document.querySelectorAll('.container__rhomb');
+// 	let texts = document.querySelectorAll('.container__text');
+//  	rhombes.forEach(rhomb => rhomb.classList.remove('container__rhomb-change'));
+//  	texts.forEach(text => text.classList.remove('container__text-change'));
+// };
+
+// let containers = document.querySelectorAll('.general');
+// containers.forEach((container) => container.addEventListener('mouseover', cursorPlaceOnLevel));
+// containers.forEach((container) => container.addEventListener('mouseout', cursorRemoveOnLevel));
 
 let containers = document.querySelectorAll('.general');
+let rhomb = document.querySelector('.container__rhomb');
+let text = document.querySelector('.container__text');
+
+let cursorPlaceOnLevel = event => {
+	event.currentTarget.classList.add('container__text-change');
+};
+let cursorRemoveOnLevel = event => {
+	event.currentTarget.classList.remove('container__text-change');
+};
 containers.forEach((container) => container.addEventListener('mouseover', cursorPlaceOnLevel));
 containers.forEach((container) => container.addEventListener('mouseout', cursorRemoveOnLevel));
 
@@ -19,8 +32,4 @@ let button = document.querySelector('.start__button');
 button.addEventListener('mouseover', event => event.target.classList.add('start__button-change'));
 button.addEventListener('mouseout', event => event.target.classList.remove('start__button-change'));
 
-// general.addEventListener('mouseover', cursorPlace);
-// containers.forEach.call(function(container) {
-// 	container.addEventListener('mouseover', cursorPlace);
-// })
 
